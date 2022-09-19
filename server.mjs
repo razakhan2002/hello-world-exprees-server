9cb0b42c72b60f7f84115774bf18d67aeeb7e915
KhurramMustaqeem <itzkhurram367@gmail.com>
2022-09-19 20:00:06 +0500

    hello world express

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port =process.env.port || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Raza Kha')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
